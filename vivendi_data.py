535b70fade6041823078ac10f5ae5b7e69809a5a
andriy-doroshchuk <adoroshchuk@gmail.com>
2025-01-08 01:11:38 +0000

    update columns calculation

--- vivendi_data.py
+++ vivendi_data.py
@@ -5,9 +5,15 @@ import pandas
 import yfinance
 from typing import Sequence, Tuple
 
-def get_exchange_rate(base: str, target: str, date: pandas.Timestamp = None) -> float:
-    base = base.lower()
-    target = target.lower()
+STOCK = {
+    "VIV.PA"    : { "currency" : "EUR", "multiplier" : 1,   "name" : "Vivendi SE" },
+    "HAVAS.AS"  : { "currency" : "EUR", "multiplier" : 1,   "name" : "Havas N.V" },
+    "CAN.L"     : { "currency" : "GBP", "multiplier" : 0.01,"name" : "Canal+ SA" },
+    "ALHG.PA"   : { "currency" : "EUR", "multiplier" : 1,   "name" : "Louis Hachette Group S.A." }
+}
+
+def get_exchange_rate(currency: str, date: pandas.Timestamp = None) -> float:
+    base, target = currency.lower().split('.')
     date = date.strftime('%Y-%m-%d') if date is not None else 'latest'
     urls = [
         f'https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@{date}/v1/currencies/{base}.json',
@@ -23,22 +29,13 @@ def get_exchange_rate(base: str, target: str, date: pandas.Timestamp = None) -> 
     return 0.0
 
 def calc_day_value(row):
-    print(row.keys())
+    value = 0
+    for s in STOCK.keys():
+        currency = STOCK[s]['currency']
+        value += row[s] * STOCK[s]['multiplier'] * row[f'{currency}.AUD']
+    return value
 
 class VivendiStock:
-    stock_names = {
-        "VIV.PA": "Vivendi SE",
-        "HAVAS.AS": "Havas N.V",
-        "CAN.L": "Canal+ SA",
-        "ALHG.PA": "Louis Hachette Group S.A.",
-    }
-    stock_currency = {
-        "VIV.PA": "EUR",
-        "HAVAS.AS": "EUR",
-        "CAN.L": "GBP",
-        "ALHG.PA": "EUR",
-    }
-
     def __init__(self):
         data_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data', 'storage.json')
 
@@ -63,11 +60,11 @@ class VivendiStock:
             json.dump(json_data, f, sort_keys = True, indent = 4, separators=(',', ' : '))
 
     def keys(self) -> Sequence:
-        return self.stock_names.keys()
+        return STOCK.keys()
 
     def name(self, id: str) -> str:
         try:
-            return self.stock_names[id]
+            return STOCK[id]['name']
         except Exception:
             return '<unknown>'
 
@@ -91,31 +88,30 @@ class VivendiStock:
             return 0, 0
         
     def _update_data(self, current_data: pandas.DataFrame = None) -> pandas.DataFrame:
-        latest_data = yfinance.download([k for k in self.stock_names.keys()],
+        currencies = ('EUR.AUD', 'GBP.AUD')
+
+        latest_data = yfinance.download([k for k in STOCK.keys()],
                                         interval="1d", start="2024-12-15", rounding=True).Close
-        
+
         if current_data is None:
             zero_values = [0.0] * len(latest_data.index)
-            latest_data = latest_data.assign(EUR = pandas.Series(zero_values, index = latest_data.index))
-            latest_data = latest_data.assign(GBP = pandas.Series(zero_values, index = latest_data.index))
-            current_data = latest_data
+            kwargs = { k : zero_values for k in currencies }
         elif len(current_data.index) < len(latest_data.index):
             zero_values = [0.0] * (len(latest_data.index) - len(current_data.index))
-            latest_data = latest_data.assign(EUR = pandas.Series(current_data['EUR'].values + zero_values, index = latest_data.index))
-            latest_data = latest_data.assign(GBP = pandas.Series(current_data['GBP'].values + zero_values, index = latest_data.index))
-            current_data = latest_data
-        
+            kwargs = { k : current_data[k].values + zero_values for k in currencies }
+      
+        kwargs = { k : pandas.Series(kwargs[k], index = latest_data.index) for k in kwargs }
+        current_data = latest_data.assign(**kwargs)
+
         # initialize exchange data
-        for curr in ('EUR', 'GBP'):
+        for curr in currencies:
             for d in current_data.index:
                 rate = current_data[curr][d]
                 if rate == 0.0:
-                    current_data.loc[d, curr] = get_exchange_rate(curr, 'AUD', d)
+                    current_data.loc[d, curr] = get_exchange_rate(curr, d)
 
         # recalculate stock value
-        stock_value = [0.0] * len(current_data.index)
-        kwargs = { 'VAL.1000' : pandas.Series(stock_value, index = current_data.index) }
-        kwargs = { 'VAL.1000' : lambda row: calc_day_value(row) }
+        kwargs = { 'AUD.VALUE' : lambda row: calc_day_value(row) }
         current_data = current_data.assign(**kwargs)
 
         return current_data

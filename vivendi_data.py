6213507d8c5f08a5accd606f75569e1a7dff5177
Andriy Doroshchuk <adoroshchuk@gmail.com>
2025-05-10 23:08:38 +1000

    update webapi to use alphavantage

--- vivendi_data.py
+++ vivendi_data.py
@@ -1,11 +1,14 @@
 import os
-import json
 import datetime
-import requests
 import pandas
-import yfinance
-from typing import Sequence, Tuple
 
+from typing import Tuple
+from web_api import download_stock_data, download_exchange_rate
+from web_api import load_cached_data, save_cached_data
+
+
+DATA_STORAGE = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data')
+CURRENCIES = ('EUR.AUD', 'GBP.AUD')
 STOCK = {
     'VIV.PA': {'currency': 'EUR', 'multiplier': 1,   'name': 'Vivendi SE'},
     'HAVAS.AS': {'currency': 'EUR', 'multiplier': 1,   'name': 'Havas N.V'},
@@ -14,58 +17,54 @@ STOCK = {
 }
 
 
-def get_exchange_rate(currency: str, date: pandas.Timestamp = None) -> float:
-    base, target = currency.lower().split('.')
-    date = date.strftime('%Y-%m-%d') if date is not None else 'latest'
-    urls = [
-        f'https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@{date}/v1/currencies/{base}.json',
-        f'https://{date}.currency-api.pages.dev/v1/currencies/{base}.json'
-    ]
-    for url in urls:
-        try:
-            response = requests.get(url)
-            parsed_response = response.json()
-            return parsed_response[base][target]
-        except Exception:
-            pass
-    return 0.0
+def update_stock_data(current_data: pandas.DataFrame, new_data: pandas.DataFrame) -> pandas.DataFrame:
+    def update_exchange_rate(data: pandas.DataFrame) -> pandas.DataFrame:
+        for curr in CURRENCIES:
+            for day in data.index:
+                rate = data[curr][day]
+                if rate == 0.0:
+                    data.loc[day, curr] = download_exchange_rate(curr, day)
+        return data
 
+    def calc_day_value(row) -> float:
+        value = 0
+        for s in STOCK.keys():
+            currency = STOCK[s]['currency']
+            value += row[s] * STOCK[s]['multiplier'] * row[f'{currency}.AUD']
+        return round(value, 3)
 
-def calc_day_value(row):
-    value = 0
-    for s in STOCK.keys():
-        currency = STOCK[s]['currency']
-        value += row[s] * STOCK[s]['multiplier'] * row[f'{currency}.AUD']
-    return round(value, 3)
+    if current_data is None:
+        current_data = new_data.combine(pandas.DataFrame(0, index=new_data.index, columns=CURRENCIES),
+                                        pandas.Series.combine_first)
+    else:
+        current_data = current_data.combine(new_data, pandas.Series.combine_first)
+
+    # ensure that exchange rate values are present
+    current_data = update_exchange_rate(current_data.fillna(0))
+
+    # recalculate stock value
+    kwargs = {'AUD.VALUE': lambda row: calc_day_value(row)}
+    current_data = current_data.assign(**kwargs)
+
+    return current_data
 
 
 class VivendiStock:
-    def __init__(self):
-        data_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data', 'storage.json')
+    def __init__(self, api_key: str):
+        self.api_key = api_key
+        self.data = load_cached_data('cache.json')
+        self.last_update = self.data.last_valid_index()
+        self.update()
 
-        self.data = None
-
-        # load the data from a file (if present)
-        try:
-            if os.path.isfile(data_file):
-                with open(data_file, encoding='utf8') as f:
-                    self.data = pandas.read_json(f)
-        except Exception as e:
-            print(f'error: {e}')
-            self.data = None
-
-        # update data from providers
-        self.last_update = datetime.datetime.now()
-        if self.data is None or \
-                self.last_update - self.data.index.max().to_pydatetime() > datetime.timedelta(days=1):
-            self.data = self._update_data(self.data)
-
-        # convert and save
-        json_data = json.loads(self.data.to_json(date_format='iso'))
-        if not os.path.isdir(os.path.dirname(data_file)):
-            os.makedirs(os.path.dirname(data_file))
-        with open(data_file, mode='wt', encoding='utf8') as f:
-            json.dump(json_data, f, sort_keys=True, indent=4, separators=(',', ' : '))
+    def update(self):
+        now = datetime.datetime.now()
+        should_update = self.last_update is None
+        should_update = should_update or self.last_update < now - datetime.timedelta(days=1)
+        if should_update:
+            self.last_update = now
+            new_data = download_stock_data(self.api_key, STOCK.keys(), CURRENCIES)
+            self.data = update_stock_data(self.data, new_data)
+            save_cached_data(self.data, 'cache.json')
 
     def get_data(self, id: str) -> Tuple[pandas.Series, float, float]:
         try:
@@ -78,44 +77,9 @@ class VivendiStock:
         except Exception:
             return pandas.Series(index=self.data.index), 0, 0
 
-    def update(self):
-        if datetime.datetime.now() - self.last_update > datetime.timedelta(days=1):
-            self.data = self._update_data(self.data)
-
-    def _update_data(self, current_data: pandas.DataFrame = None) -> pandas.DataFrame:
-        currencies = ('EUR.AUD', 'GBP.AUD')
-
-        latest_data = yfinance.download([k for k in STOCK.keys()],
-                                        interval='1d', start='2024-12-15', rounding=True).Close
-
-        if current_data is None:
-            zero_values = [0.0] * len(latest_data.index)
-            kwargs = {k: zero_values for k in currencies}
-        elif len(current_data.index) < len(latest_data.index):
-            zero_values = [0.0] * (len(latest_data.index) - len(current_data.index))
-            kwargs = {k: (current_data[k].to_list() + zero_values) for k in currencies}
-        else:
-            kwargs = {k: current_data[k].to_list() for k in currencies}
-
-        kwargs = {k: pandas.Series(kwargs[k], index=latest_data.index) for k in kwargs}
-        current_data = latest_data.assign(**kwargs)
-
-        # initialize exchange data
-        for curr in currencies:
-            for d in current_data.index:
-                rate = current_data[curr][d]
-                if rate == 0.0:
-                    current_data.loc[d, curr] = get_exchange_rate(curr, d)
-
-        # recalculate stock value
-        kwargs = {'AUD.VALUE': lambda row: calc_day_value(row)}
-        current_data = current_data.assign(**kwargs)
-
-        return current_data
-
 
 if __name__ == '__main__':
-    stock = VivendiStock()
+    stock = VivendiStock(api_key=os.getenv('ALPHAVANTAGE_API_KEY'))
     print(stock.data)
     _, curr_price, change = stock.get_data('HAVAS.AS')
-    print (curr_price, change)
+    print(curr_price, change)
---
+++ web_api.py
@@ -0,0 +1,121 @@
+import os
+import json
+import requests
+import pandas
+
+
+DATA_STORAGE = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data')
+
+
+def load_json_data(file_name: str) -> dict:
+    data_file = os.path.join(DATA_STORAGE, file_name)
+    try:
+        if os.path.isfile(data_file):
+            with open(data_file, encoding='utf8') as f:
+                return json.load(f)
+    except Exception as e:
+        print(f'error: {e}')
+    return {}
+
+
+def load_cached_data(file_name: str) -> pandas.DataFrame:
+    data_file = os.path.join(DATA_STORAGE, file_name)
+    try:
+        if os.path.isfile(data_file):
+            with open(data_file, encoding='utf8') as f:
+                return pandas.read_json(f)
+    except Exception as e:
+        print(f'error: {e}')
+    return pandas.DataFrame()
+
+
+def save_json_data(data: dict, file_name: str) -> None:
+    data_file = os.path.join(DATA_STORAGE, file_name)
+    try:
+        if not os.path.isdir(os.path.dirname(data_file)):
+            os.makedirs(os.path.dirname(data_file))
+        with open(data_file, mode='wt', encoding='utf8') as f:
+            json.dump(data, f, sort_keys=True, indent=4, separators=(',', ' : '))
+    except Exception as e:
+        print(f'error: {e}')
+
+
+def save_cached_data(data: pandas.DataFrame, file_name: str) -> None:
+    save_json_data(json.loads(data.to_json(date_format='iso')), file_name)
+
+
+def __download_query(api_key: str, function: str, query_id: str) -> dict:
+    url = f'https://www.alphavantage.co/query?function={function}&outputsize=compact&datatype=json&apikey={api_key}'
+
+    data = load_json_data(f'{query_id}.json')
+    if data:
+        print(f'using cached data for {query_id}')
+    else:
+        response = requests.get(url)
+        data = response.json()
+
+    save_json_data(data, f'{query_id}.json')
+    return data
+
+
+def __download_stock_symbol(api_key: str, symbol: str) -> dict:
+    return __download_query(api_key, f'TIME_SERIES_DAILY&symbol={symbol}', symbol)
+
+
+def __download_exchange_pair(api_key: str, exchange_pair: str) -> dict:
+    from_symbol, to_symbol = exchange_pair.split('.')
+    return __download_query(api_key, f'FX_DAILY&from_symbol={from_symbol}&to_symbol={to_symbol}', exchange_pair)
+
+
+def download_stock_data(api_key: str, stock_symbols, currency_pairs) -> pandas.DataFrame:
+    def get_daily_close_price(series: str, data: dict) -> dict:
+        def get_day_close_price(day_data: dict) -> float:
+            for key in day_data:
+                if 'close' in key.lower():
+                    return float(day_data[key])
+            return 0.0
+
+        try:
+            time_series = data[series]
+            return {day: get_day_close_price(time_series[day]) for day in time_series}
+        except:
+            return {}
+
+    try:
+        # download stock data
+        stock_data = {symbol: get_daily_close_price('Time Series (Daily)', __download_stock_symbol(api_key, symbol))
+                      for symbol in stock_symbols}
+        exchange_data = {currency: get_daily_close_price('Time Series FX (Daily)', __download_exchange_pair(api_key, currency))
+                         for currency in currency_pairs}
+        stock_data.update(exchange_data)
+        # ensure that all stock data has the same dates
+        dates = set()
+        for symbol in stock_data:
+            dates.update(stock_data[symbol].keys())
+        dates = sorted(dates, reverse=True)
+        stock_data = {symbol: {date: stock_data[symbol].get(date, 0.0) for date in dates} for symbol in stock_data}
+        # convert to DataFrame
+        return pandas.DataFrame().from_dict(stock_data, orient='columns')
+    except Exception as e:
+        print(f'error: {e}')
+        return pandas.DataFrame()
+
+
+def download_exchange_rate(currency: str, date: pandas.Timestamp = None) -> float:
+    base, target = currency.lower().split('.')
+    if date is None:
+        date = 'latest'
+    elif isinstance(date, pandas.Timestamp):
+        date = date.strftime('%Y-%m-%d')
+    urls = [
+        f'https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@{date}/v1/currencies/{base}.json',
+        f'https://{date}.currency-api.pages.dev/v1/currencies/{base}.json'
+    ]
+    for url in urls:
+        try:
+            response = requests.get(url)
+            parsed_response = response.json()
+            return parsed_response[base][target]
+        except:
+            pass
+    return 0.0
